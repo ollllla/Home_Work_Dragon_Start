40bd82e5124d07c1160cd4396c69f0e09d69e826
ollllla <ola.sheremeta@gmail.com>
2018-11-12 20:58:45 +0200

    $ git commit -m "first commit"

---
+++ Bubble_Sort_Even_Odd.py
@@ -0,0 +1,27 @@
+list = [64, 34, 25, 12, 22, 11, 90, 15, 17, 5, 8]
+
+def bubbleSort(list):
+    n = len(list)
+
+    # Traverse through all array elements
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # Swap if the element found is greater
+            # than the next element
+            if list[j] > list[j+1] :
+                list[j], list[j+1] = list[j+1], list[j]
+
+bubbleSort(list)
+
+evens = []
+odds = []
+
+for i in list:
+    if(i%2==0):
+        evens.append(str(i))
+    else:
+        odds.append(str(i))
+
+print("Result: {}" .format(evens+odds))
